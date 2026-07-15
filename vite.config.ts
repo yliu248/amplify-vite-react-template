0afffd0bac23ba60025e03612d2ccd3d2edc03fd
yliu248 <68000614+yliu248@users.noreply.github.com>
2026-07-15 06:04:34 +0000

    Fix: Node 20 + resolve aws-amplify Rollup import error

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      '@aws-amplify/core/internals/utils'
+    ]
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/]
+    }
+  }
 })
